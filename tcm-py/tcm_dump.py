47a280330e8afe4294c513498b70fb45a47a0119
Nicholas Bellinger <nab@linux-iscsi.org>
2009-07-19 20:50:03 -0700

    [tcm-py]: Add ALUA group ops to tcm_node.py and logic to tcm_dump.py
    This patch adds the following ALUA groups ops for adding, setting, listing and
    deleting of ALUA LU Groups, and adding, listing and deleting ALUA Target
    Port Groups to tcm_node.py:
    
      --addlungp=LU_GP_NAME
                            Add ALUA Logical Unit Group
      --addtgptgp=TG_PT_GP_NAME
                            Add ALUA Target Port Group
      --dellungp=LU_GP_NAME
                            Delete ALUA Logical Unit Group
      --deltgptgp=TG_PT_GP_NAME
                            Delete ALUA Target Port Group
      --listlugps           List ALUA Logical Unit Groups
      --listtgptgps         List ALUA Target Port Groups
      --setlugp=HBA/DEV     Set ALUA Logical Unit Group
    
    This patch also updates tcm_node.py:tcm_unload() to call tcm_del_alua_tgptgp()
    and tcm_del_alua_lugp() to remove existing ALUA groups during shutdown.
    
    Also, tcm_dump.py has been updated to output running ALUA group information that
    is used during target_core_mod restart.
    
    Signed-off-by: Nicholas A. Bellinger <nab@linux-iscsi.org>

--- tcm-py/tcm_dump.py
+++ tcm-py/tcm_dump.py
@@ -19,12 +19,41 @@ def tcm_dump_hba_devices():
 
 def tcm_dump_configfs():
 
-	hba_root = os.listdir(tcm_root)
-
 	print "modprobe target_core_mod"
 
+	# Loop through ALUA Logical Unit and Target Port groups
+	# Note that the 'default_lu_gp' and 'default_tg_pt_gp' are automatically
+	# created when target_core_mod is loaded.
+	print "#### ALUA Logical Unit Groups"
+	for lu_gp in os.listdir(tcm_root + "/alua/lu_gps"):	
+		if lu_gp == "default_lu_gp":
+			continue
+
+		print "mkdir -p " + tcm_root + "/alua/lu_gps/" + lu_gp
+		lu_gp_id_file = tcm_root + "/alua/lu_gps/" + lu_gp + "/lu_gp_id"
+		p = os.open(lu_gp_id_file, 0)
+		value = os.read(p, 8)
+		os.close(p)
+		if not value:
+			continue
+		print "echo " + value.rstrip() + " > " + lu_gp_id_file
+
+	print "#### ALUA Target Port Groups"
+	for tg_pt_gp in os.listdir(tcm_root + "/alua/tg_pt_gps"):
+		if tg_pt_gp == "default_tg_pt_gp":
+			continue
+
+		print "mkdir -p " + tcm_root + "/alua/tg_pt_gps/" + tg_pt_gp
+		tg_pt_gp_id_file = tcm_root + "/alua/tg_pt_gps/" + tg_pt_gp + "/tg_pt_gp_id"
+		p = os.open(tg_pt_gp_id_file, 0)
+		value = os.read(p, 8)
+		os.close(p)
+		if not value:
+			continue
+		print "echo " + value.rstrip() + " > " + tg_pt_gp_id_file
+
 	# Loop through HBA list
-	for f in hba_root:
+	for f in os.listdir(tcm_root):
 		if f == "alua":
 			continue;
 		
@@ -82,6 +111,22 @@ def tcm_dump_configfs():
 				unit_serial = value[off:]
 				print "echo " + unit_serial.rstrip() + " > " + unit_serial_file
 				os.close(p)
+
+			# Dump ALUA Logical Unit Group
+			lu_gp_file = dev_root + g + "/alua_lu_gp"
+			p = os.open(lu_gp_file, 0)
+			value = os.read(p, 512)
+			os.close(p)
+			if value:
+				lu_gp_tmp = value.split('\n')
+				lu_gp_out = lu_gp_tmp[0]
+				off = lu_gp_out.index('Alias: ')
+				off += 7 # Skip over "Alias: "
+				lu_gp_name = lu_gp_out[off:]
+				# Only need to dump if storage object is NOT part of
+				# the 'default_lu_gp'
+				if not re.search(lu_gp_name, 'default_lu_gp'):
+					print "echo " + lu_gp_name + " > " + lu_gp_file
 
 			# Dump device attributes
 			print "#### Attributes for " + dev_root + g
--- tcm-py/tcm_node.py
+++ tcm-py/tcm_node.py
@@ -23,6 +23,50 @@ def tcm_get_cfs_prefix(arg):
 	path = "/sys/kernel/config/target/core/" + arg
 	return path
 
+def tcm_add_alua_lugp(option, opt_str, value, parser):
+	lu_gp_name = str(value)
+
+	if os.path.isdir(tcm_root + "/alua/lu_gps/" + lu_gp_name):
+		tcm_err("ALUA Logical Unit Group: " + lu_gp_name + " already exists!")
+	
+	mkdir_op = "mkdir -p " + tcm_root + "/alua/lu_gps/" + lu_gp_name
+	ret = os.system(mkdir_op)
+	if ret:
+		tcm_err("Unable to create ALUA Logical Unit Group: " + lu_gp_name)
+
+	set_lu_gp_op = "echo 0 > " + tcm_root + "/alua/lu_gps/" + lu_gp_name + "/lu_gp_id"
+	ret = os.system(set_lu_gp_op)
+	if ret:
+		rmdir_op = "rmdir " + tcm_root + "/alua/lu_gps/" + lu_gp_name
+		test = os.system(rmdir_op)
+		tcm_err("Unable to set ID for ALUA Logical Unit Group: " + lu_gp_name)
+	else:
+		print "Successfully created ALUA Logical Unit Group: " + lu_gp_name
+
+	return
+
+def tcm_add_alua_tgptgp(option, opt_str, value, parser):
+	tg_pt_gp_name = str(value)
+
+	if os.path.isdir(tcm_root + "/alua/tg_pt_gps/" + tg_pt_gp_name):
+		tcm_err("ALUA Target Port Group: " + tg_pt_gp_name + " already exists!")
+
+	mkdir_op = "mkdir -p " + tcm_root + "/alua/tg_pt_gps/" + tg_pt_gp_name
+	ret = os.system(mkdir_op)
+	if ret:
+		tcm_err("Unable to create ALUA Target Port Group: " + tg_pt_gp_name)
+
+	set_tg_pt_gp_op = "echo 0 > " + tcm_root + "/alua/tg_pt_gps/" + tg_pt_gp_name + "/tg_pt_gp_id"
+	ret = os.system(set_tg_pt_gp_op)
+	if ret:
+		rmdir_op = "rmdir " + tcm_root + "/alua/tg_pt_gps/" + tg_pt_gp_name
+		os.system(rmdir_op)
+		tcm_err("Unable to set ID for ALUA Target Port Group: " + tg_pt_gp_name)
+	else:
+		print "Successfully created ALUA Target Port Group: " + tg_pt_gp_name
+
+	return
+
 def tcm_delhba(option, opt_str, value, parser):
 	hba_name = str(value)
 
@@ -42,6 +86,36 @@ def tcm_delhba(option, opt_str, value, parser):
 		tcm_err("Unable to delete TCM HBA: " + hba_path)
 	else:
 		print "Successfully released TCM HBA: " + hba_path
+
+	return
+
+def tcm_del_alua_lugp(option, opt_str, value, parser):
+	lu_gp_name = str(value)
+
+	if not os.path.isdir(tcm_root + "/alua/lu_gps/" + lu_gp_name):
+		tcm_err("ALUA Logical Unit Group: " + lu_gp_name + " does not exist!")
+
+	rmdir_op = "rmdir "  + tcm_root + "/alua/lu_gps/" + lu_gp_name
+	ret = os.system(rmdir_op)
+	if ret:
+		tcm_err("Unable to delete ALUA Logical Unit Group: " + lu_gp_name)
+	else:
+		print "Successfully deleted ALUA Logical Unit Group: " + lu_gp_name
+
+	return
+
+def tcm_del_alua_tgptgp(option, opt_str, value, parser):
+	tg_pt_gp_name = str(value)
+
+	if not os.path.isdir(tcm_root + "/alua/tg_pt_gps/" + tg_pt_gp_name):
+		tcm_err("ALUA Target Port Group: " + tg_pt_gp_name + " does not exist!")
+
+	rmdir_op = "rmdir " + tcm_root + "/alua/tg_pt_gps/" + tg_pt_gp_name
+	ret = os.system(rmdir_op)
+	if ret:
+		tcm_err("Unable to delete ALUA Target Port Group: " + tg_pt_gp_name)
+	else:
+		print "Successfully deleted ALUA Target Port Group: " + tg_pt_gp_name
 
 	return
 
@@ -186,6 +260,56 @@ def tcm_list_hbas(option, opt_str, value, parser):
 
 	return
 
+def tcm_list_alua_lugps(option, opt_str, value, parser):
+
+	for lu_gp in os.listdir(tcm_root + "/alua/lu_gps"):
+		p = os.open(tcm_root + "/alua/lu_gps/" + lu_gp + "/lu_gp_id", 0)
+		value = os.read(p, 8)
+		lu_gp_id = value.rstrip();
+		os.close(p)
+		print "\------> " + lu_gp + "  LUN Group ID: " + lu_gp_id
+
+		p = os.open(tcm_root + "/alua/lu_gps/" + lu_gp + "/members", 0)
+		value = os.read(p, 4096)
+		lu_gp_members = value.split('\n');
+		os.close(p)
+
+		if len(lu_gp_members) == 1:
+			print "         No Logical Unit Group Members"
+			continue
+
+		i = 0
+		while i < len(lu_gp_members) - 1:
+			print "         " + lu_gp_members[i]
+			i += 1
+
+	return
+
+def tcm_list_alua_tgptgps(option, opt_str, value, parser):
+
+	for tg_pt_gp in os.listdir(tcm_root + "/alua/tg_pt_gps"):
+		p = os.open(tcm_root + "/alua/tg_pt_gps/" + tg_pt_gp + "/tg_pt_gp_id", 0)
+		value = os.read(p, 8)
+		tg_pt_gp_id = value.rstrip()
+		os.close(p)
+		print "\------> " + tg_pt_gp + "  Target Port Group ID: " + tg_pt_gp_id
+
+		p = os.open(tcm_root + "/alua/tg_pt_gps/" + tg_pt_gp + "/members", 0)
+		value = os.read(p, 4096)
+		tg_pt_gp_members = value.split('\n');
+		os.close(p)
+
+		if len(tg_pt_gp_members) == 1:
+			print "         No Target Port Group Members"
+			continue
+
+		i = 0
+		while i < len(tg_pt_gp_members) - 1:
+			print "         " + tg_pt_gp_members[i]
+			i += 1
+
+        return
+
 def tcm_show_persistent_reserve_info(option, opt_str, value, parser):
 	cfs_unsplit = str(value)
 	cfs_dev_path = tcm_get_cfs_prefix(cfs_unsplit)
@@ -196,6 +320,25 @@ def tcm_show_persistent_reserve_info(option, opt_str, value, parser):
 	ret = os.system(pr_show_op)
 	if ret:
 		tcm_err("Unable to disable storage object persistent reservation info")
+
+	return
+
+def tcm_set_alua_lugp(option, opt_str, value, parser):
+	cfs_unsplit = str(value[0])
+	cfs_dev_path = tcm_get_cfs_prefix(cfs_unsplit)
+	if (os.path.isdir(cfs_dev_path) == False):
+		tcm_err("TCM/ConfigFS storage object does not exist: " + cfs_dev_path)
+
+	lu_gp_name = str(value[1])
+	if not os.path.isdir(tcm_root + "/alua/lu_gps/" + lu_gp_name):
+		tcm_err("ALUA Logical Unit Group: " + lu_gp_name + " does not exist!")
+
+	lu_gp_set_op = "echo " + lu_gp_name + " > " + cfs_dev_path + "/alua_lu_gp"
+	ret = os.system(lu_gp_set_op)
+	if ret:
+		tcm_err("Unable to set ALUA Logical Unit Group: " + lu_gp_name + " for TCM storage object: " + cfs_dev_path)
+	else:
+		print "Successfully set ALUA Logical Unit Group: " + lu_gp_name + " for TCM storage object: " + cfs_dev_path
 
 	return
 
@@ -263,6 +406,18 @@ def tcm_unload(option, opt_str, value, parser):
 
 		tcm_delhba(None, None, f, None)
 
+	for tg_pt_gp in os.listdir(tcm_root + "/alua/tg_pt_gps"):
+		if tg_pt_gp == "default_tg_pt_gp":
+			continue
+
+		tcm_del_alua_tgptgp(None, None, tg_pt_gp, None)		
+
+	for lu_gp in os.listdir(tcm_root + "/alua/lu_gps"):
+		if lu_gp == "default_lu_gp":
+			continue
+
+		tcm_del_alua_lugp(None, None, lu_gp, None)
+
 	rmmod_op = "rmmod target_core_mod"
 	ret = os.system(rmmod_op)
 	if ret:
@@ -278,16 +433,30 @@ def tcm_version(option, opt_str, value, parser):
 def main():
 
 	parser = OptionParser()
+	parser.add_option("--addlungp", action="callback", callback=tcm_add_alua_lugp, nargs=1,
+			type="string", dest="lu_gp_name", help="Add ALUA Logical Unit Group")
+	parser.add_option("--addtgptgp", action="callback", callback=tcm_add_alua_tgptgp, nargs=1,
+			type="string", dest="tg_pt_gp_name", help="Add ALUA Target Port Group")
 	parser.add_option("--delhba", action="callback", callback=tcm_delhba, nargs=1,
 			type="string", dest="HBA", help="Delete TCM Host Bus Adapter (HBA)")
+	parser.add_option("--dellungp", action="callback", callback=tcm_del_alua_lugp, nargs=1,
+			type="string", dest="lu_gp_name", help="Delete ALUA Logical Unit Group")
+	parser.add_option("--deltgptgp", action="callback", callback=tcm_del_alua_tgptgp, nargs=1,
+			type="string", dest="tg_pt_gp_name", help="Delete ALUA Target Port Group")
 	parser.add_option("--createdev", action="callback", callback=tcm_createvirtdev, nargs=2,
 			type="string", dest="HBA/DEV <params>", help="Create TCM Storage Object")
 	parser.add_option("--freedev", action="callback", callback=tcm_freevirtdev, nargs=1,
 			type="string", dest="HBA/DEV", help="Free TCM Storage Object")
 	parser.add_option("--listhbas", action="callback", callback=tcm_list_hbas, nargs=0,
 			help="List TCM Host Bus Adapters (HBAs)")
+        parser.add_option("--listlugps", action="callback", callback=tcm_list_alua_lugps, nargs=0,
+                        help="List ALUA Logical Unit Groups")
+        parser.add_option("--listtgptgps", action="callback", callback=tcm_list_alua_tgptgps, nargs=0,
+                        help="List ALUA Target Port Groups");
 	parser.add_option("--pr", action="callback", callback=tcm_show_persistent_reserve_info, nargs=1,
 			type="string", dest="HBA/DEV", help="Show Persistent Reservation info")
+	parser.add_option("--setlugp", action="callback", callback=tcm_set_alua_lugp, nargs=2,
+			type="string", dest="HBA/DEV", help="Set ALUA Logical Unit Group")
 	parser.add_option("--setudevpath", action="callback", callback=tcm_set_udev_path, nargs=2,
 			type="string", dest="HBA/DEV <udev_path>", help="Set UDEV Path Information, only used when --createdev did not contain <udev_path> as parameter")
 	parser.add_option("--setunitserial", action="callback", callback=tcm_set_wwn_unit_serial, nargs=2,
