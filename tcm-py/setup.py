ecaa1f842a68b7d8d7e5a95e2d56e92a4d17c852
Nicholas Bellinger <nab@linux-iscsi.org>
2009-09-24 07:41:40 -0700

    [tcm-py]: Cleanups for setup.py
    setup.py: add a initial "long" package description
    setup.py: use a sensible name and version string
    setup.py: set x bit and add shebang
    
    Signed-off-by: Florian Haas <florian.haas@linbit.com>
    Signed-off-by: Nicholas A. Bellinger <nab@linux-iscsi.org>

--- tcm-py/setup.py
+++ tcm-py/setup.py
@@ -1,9 +1,13 @@
-### setup.py ###
+#!/usr/bin/python
 from distutils.core import setup
 
-setup (name='Target_Core_Mod/ConfigFS v3.0 CLI',
-      version='1.0',
+long_desc="""Python CLI for controlling the generic target_core_mod/ConfigFS engine
+"""
+
+setup (name='tcm',
+      version='3.2',
       description='CLI for controlling target_core_mod/ConfigFS',
+      long_description=long_desc,
       author='Nicholas A. Bellinger',
       author_email='nab@linux-iscsi.org',
       url='http://linux-iscsi.org',
