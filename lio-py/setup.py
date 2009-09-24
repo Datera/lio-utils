7a562d27b03c3c9fc511795963472a376f2ec453
Nicholas Bellinger <nab@linux-iscsi.org>
2009-09-24 07:44:24 -0700

    [lio-utils]: Cleanups for setup.py
    setup.py: add a initial "long" package description
    setup.py: use a sensible name and version string
    setup.py: set x bit and add shebang
    
    Signed-off-by: Florian Haas <florian.haas@linbit.com>
    Signed-off-by: Nicholas A. Bellinger <nab@linux-iscsi.org>

--- lio-py/setup.py
+++ lio-py/setup.py
@@ -1,13 +1,19 @@
-### setup.py ###
+#!/usr/bin/python
+
 from distutils.core import setup
 
-setup (name='LIO-Target/ConfigFS v3.0 CLI',
-      version='1.0',
+long_desc="""Python CLI for controlling the LIO-Target/ConfigFS fabric module
+"""
+
+setup(name='lio',
+      version='3.2',
       description='CLI for controlling LIO-Target/ConfigFS',
+      long_description=long_desc,
       author='Nicholas A. Bellinger',
       author_email='nab@linux-iscsi.org',
       url='http://linux-iscsi.org',
       license='GPL',
+      requires=['tcm'],
       py_modules=['lio_dump','lio_node']
 )
 
