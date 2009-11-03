d53f2eaf7d1aae569d042329619be962567cc077
root <root@lio-sles.risingtidesystems.com>
2009-11-02 18:50:26 -0800

    [get-py-modules-path.py]: Make local python modules path locate slightly more sane
    This patch makes get-py-modules-path.py to do the right thing in searching
    for 'python2.6' from get_python_lib() output instead of 'dist-packages', as
    this may be different between distributions that support Python v2.6.
    
    Signed-off-by: Nicholas A. Bellinger <nab@linux-iscsi.org>
    (cherry picked from commit 5ed92e19ca8ac49666b3949daf9d65272dbbf312)

--- get-py-modules-path.py
+++ get-py-modules-path.py
@@ -5,7 +5,7 @@ from distutils.sysconfig import get_python_lib,re;
 str = get_python_lib()
 
 # Fix up get_python_lib() path for Python v2.6 on Ubuntu 9.10 and SLES 11
-if re.search('dist-packages', str):
+if re.search('python2.6', str):
 	print str.replace('/usr','/usr/local')
 else:
 	print str;
