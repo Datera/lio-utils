9c5fa5bcfaf7bb1cd1e5c3dba5cdc42489fde354
Nicholas Bellinger <nab@linux-iscsi.org>
2010-10-13 22:58:55 -0700

    tcm-py: Add v4.0 rmmod ops for TCM subsystem plugin LKMs
    This patch updates tcm_unload() to remove the known TCM subsystem plugins
    which appear as seperate kernel modules in the lastest v4.0.0-rc code.
    
    Signed-off-by: Nicholas A. Bellinger <nab@linux-iscsi.org>

--- tcm-py/tcm_node.py
+++ tcm-py/tcm_node.py
@@ -1241,6 +1241,20 @@ def tcm_unload(option, opt_str, value, parser):
 
 		tcm_del_alua_lugp(None, None, lu_gp, None)
 
+	# Unload TCM subsystem plugin modules
+	rmmod_op = "rmmod target_core_iblock"
+	ret = os.system(rmmod_op)
+
+	rmmod_op = "rmmod target_core_file"
+	ret = os.system(rmmod_op)
+
+	rmmod_op = "rmmod target_core_pscsi"
+	ret = os.system(rmmod_op)
+
+	rmmod_op = "rmmod target_core_stgt"
+	ret = os.system(rmmod_op)
+
+	# Unload TCM Core
 	rmmod_op = "rmmod target_core_mod"
 	ret = os.system(rmmod_op)
 	if ret:
